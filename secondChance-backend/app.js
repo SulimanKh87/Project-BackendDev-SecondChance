79806234ac42a88fd44f5b7f32c1fae34db0bafc
SulimanKh87 <suli.tempmail2022@gmail.com>
2024-05-21 08:03:06 -0400

    Develop Back-end Integration for Login

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -24,6 +24,7 @@ app.use(express.json());
 
 // Route files
 const secondChanceRoutes = require('./routes/secondChanceItemsRoutes');
+const authRoutes = require('./routes/authRoutes');
 const searchRoutes = require('./routes/searchRoutes');
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
@@ -33,6 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Use Routes
 app.use('/api/secondchance/items', secondChanceRoutes);
+app.use('/api/auth', authRoutes);
 app.use('/api/secondchance/search', searchRoutes);
 
 // Global Error Handler
---
+++ secondChance-backend/routes/authRoutes.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const connectToDatabase = require('../models/db');
+const router = express.Router();
+const dotenv = require('dotenv');
+const pino = require('pino');  // Import Pino logger
+dotenv.config();
+
+const logger = pino();  // Create a Pino logger instance
+
+//Create JWT secret
+const JWT_SECRET = process.env.JWT_SECRET;
+
+router.post('/register', async (req, res) => {
+    try {
+      //Connect to `secondChance` in MongoDB through `connectToDatabase` in `db.js`.
+      const db = await connectToDatabase();
+      const collection = db.collection("users");
+      const existingEmail = await collection.findOne({ email: req.body.email });
+
+        if (existingEmail) {
+            logger.error('Email id already exists');
+            return res.status(400).json({ error: 'Email id already exists' });
+        }
+
+        const salt = await bcryptjs.genSalt(10);
+        const hash = await bcryptjs.hash(req.body.password, salt);
+        const email=req.body.email;
+        const newUser = await collection.insertOne({
+            email: req.body.email,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            password: hash,
+            createdAt: new Date(),
+        });
+
+        const payload = {
+            user: {
+                id: newUser.insertedId,
+            },
+        };
+
+        const authtoken = jwt.sign(payload, JWT_SECRET);
+        logger.info('User registered successfully');
+        res.json({ authtoken,email });
+    } catch (e) {
+        logger.error(e);
+        return res.status(500).send('Internal server error');
+    }
+});
+
+module.exports = router;
