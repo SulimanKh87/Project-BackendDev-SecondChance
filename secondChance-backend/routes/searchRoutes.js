e52675e7c4910e9674c480a9171b9c4e3817fcd7
SulimanKh87 <suli.tempmail2022@gmail.com>
2024-05-21 14:15:27 -0400

    Integrate Back End to the Front End

--- secondChance-backend/routes/searchRoutes.js
+++ secondChance-backend/routes/searchRoutes.js
@@ -1,38 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const connectToDatabase = require('../models/db');
+require('dotenv').config();
 
 // Search for gifts
 router.get('/', async (req, res, next) => {
     try {
-        // Task 1: Connect to MongoDB using connectToDatabase database. Remember to use the await keyword and store the connection in `db`
         const db = await connectToDatabase();
-
-        const collection = db.collection("gifts");
-
+        const collection = db.collection(process.env.MONGO_COLLECTION);
         // Initialize the query object
         let query = {};
 
         // Add the name filter to the query if the name parameter is not empty
-        // if (/* {{insert code here}} */) {
-        if (req.query.name && req.query.name.trim() !== ''){
+        if (req.query.name && req.query.name.trim() !== '') {
             query.name = { $regex: req.query.name, $options: "i" }; // Using regex for partial match, case-insensitive
         }
 
-        // Task 3: Add other filters to the query
+        // Add other filters to the query
         if (req.query.category) {
             query.category = req.query.category;
         }
         if (req.query.condition) {
-              query.condition = req.query.condition;
+            query.condition = req.query.condition;
         }
         if (req.query.age_years) {
             query.age_years = { $lte: parseInt(req.query.age_years) };
         }
 
-        // Task 4: Fetch filtered gifts using the find(query) method. Make sure to use await and store the result in the `gifts` constant
-        query.category = req.query.category;
-
+        const gifts = await collection.find(query).toArray();
         res.json(gifts);
     } catch (e) {
         next(e);
